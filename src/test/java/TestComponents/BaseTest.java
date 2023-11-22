de99b40fa29d05c6e7d04b6b446a4a4bb45520a3
Deep Singh <sarab_2001inn@yahoo.com>
2023-11-22 16:12:25 +0100

    New Commit

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
@@ -127,6 +128,12 @@ public class BaseTest {
 	public void teadDown()
 	{
 		driver.close();
+	}
+	
+	@AfterSuite
+	public void endTest()
+	{
+		driver.quit();
 	}
 	
 	
